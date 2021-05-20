990191cba318a9ddb6e276f60eeaa09005e51d14
Jonadngt <jonadngt@gmail.com>
2021-05-19 21:04:27 -0400

    upload

--- ejemplo.py
+++ ejemplo.py
@@ -1,3 +1,4 @@
+from json_to_date import module
 import json_to_date
 json_dir = 'person.json'
 data = json_to_date.module.get_data(json_dir)
@@ -7,3 +8,4 @@ print('Apellido: ' + data.last_name)
 print('Cumpleaños: ' + data.birth)
 print('Trabaja en: ' + data.job.name)
 print('Comenzó a trabajar el: ' + data.job.start_date)
+print('Autor: ' + json_to_date.__author__)
